eca67bd35ca6dcd8804f63554b55673137a0d767
CristianCastiglione <cristian_castiglione@libero.it>
2023-09-28 21:22:25 +0200

    Update deviance.h

--- src/deviance.h
+++ src/deviance.h
@@ -9,8 +9,8 @@
 #include <RcppArmadillo.h>
 
 // Pointwise deviance
-template<class F> void deviance (arma::mat & dev, const arma::mat & mu, const amra::mat & y, F family);
-template<class F> arma::mat deviance (const arma::mat & mu, const amra::mat & y, F family);
+template<class F> void deviance (arma::mat & dev, const arma::mat & y, const arma::mat & mu, F family);
+template<class F> arma::mat deviance (const arma::mat & y, const arma::mat & mu, F family);
 
 // Penalty function
 void penalty (double & pen, const arma::mat & u, const arma::vec & p);
