144a99a201c8f26ab31dd0135f36927cb960a169
CristianCastiglione <cristian_castiglione@libero.it>
2023-09-28 20:54:56 +0200

    Update deviance.cpp

--- src/deviance.cpp
+++ src/deviance.cpp
@@ -6,10 +6,7 @@
 #include "deviance.h"
 
 // Pointwise deviance
-template<class F> void deviance (const arma::mat & mu, const amra::mat & y, F family);
-
-template<class F> arma::mat deviance (const arma::mat & mu, const amra::mat & y, F family) {
-    arma::mat dev(arma::size(y));
+template<class F> void deviance (arma::mat & dev, const arma::mat & mu, const amra::mat & y, F family) {
     bool anyna = !y.is_finite();
     if (anyna) {
         arma::uvec notna = arma::find_finite(y);
@@ -17,6 +14,12 @@ template<class F> arma::mat deviance (const arma::mat & mu, const amra::mat & y,
     } else {
         dev = family->devresid(y, mu);
     }
+    return dev;
+};
+
+template<class F> arma::mat deviance (const arma::mat & mu, const amra::mat & y, F family) {
+    arma::mat dev(arma::size(y));
+    deviance(dev, mu, y, family);
     return dev;
 }
 
