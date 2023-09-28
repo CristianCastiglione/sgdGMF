040f35970cf196d8147c9460edb72ce154f88af6
CristianCastiglione <cristian_castiglione@libero.it>
2023-09-28 20:50:39 +0200

    Update family.cpp

--- src/family.cpp
+++ src/family.cpp
@@ -10,6 +10,9 @@ arma::mat Family::Gaussian::variance (const arma::mat & mu) const {return arma::
 arma::mat Family::Gaussian::initialize (const arma::mat & y) const {return y;}
 bool Family::Gaussian::validmu (const arma::mat & mu) const {return true;}
 bool Family::Gaussian::valideta (const arma::mat & eta) const {return true;}
+arma::mat Family::Gaussian::devresid (const arma::mat & y, const arma::mat & mu) const {
+    return arma::square(y - mu);
+}
 arma::mat Family::Gaussian::devresid (const arma::mat & y, const arma::mat & mu, const arma::mat & wt) const {
     return wt % arma::square(y - mu);
 }
@@ -19,6 +22,9 @@ arma::mat Family::Binomial::variance (const arma::mat & mu) const {return mu % (
 arma::mat Family::Binomial::initialize (const arma::mat & y) const {return (2 * y - 1) + 0.05 * arma::randu(size(y));}
 bool Family::Binomial::validmu (const arma::mat & mu) const {return arma::all(arma::all(mu > 0)) && arma::all(arma::all(mu < 1));}
 bool Family::Binomial::valideta (const arma::mat & eta) const {return true;}
+arma::mat Family::Binomial::devresid (const arma::mat & y, const arma::mat & mu) const {
+    return - 2 * (y % arma::log(mu) + (1 - y) % arma::log(1 - mu));
+}
 arma::mat Family::Binomial::devresid (const arma::mat & y, const arma::mat & mu, const arma::mat & wt) const {
     return - 2 * wt % (y % arma::log(mu) + (1 - y) % arma::log(1 - mu));
 }
@@ -28,6 +34,9 @@ arma::mat Family::Poisson::variance (const arma::mat & mu) const {return mu;}
 arma::mat Family::Poisson::initialize (const arma::mat & y) const {return this->linkfun(y + 0.1);}
 bool Family::Poisson::validmu (const arma::mat & mu) const {return arma::all(arma::all(mu > 0));}
 bool Family::Poisson::valideta (const arma::mat & eta) const {return true;}
+arma::mat Family::Poisson::devresid (const arma::mat & y, const arma::mat & mu) const {
+    return 2 * (y % arma::log(y / mu) - (y - mu));
+}
 arma::mat Family::Poisson::devresid (const arma::mat & y, const arma::mat & mu, const arma::mat & wt) const {
     return 2 * wt % (y % arma::log(y / mu) - (y - mu));
 }
@@ -37,6 +46,9 @@ arma::mat Family::Gamma::variance (const arma::mat & mu) const {return arma::squ
 arma::mat Family::Gamma::initialize (const arma::mat & y) const {return this->linkfun(y);}
 bool Family::Gamma::validmu (const arma::mat & mu) const {return arma::all(arma::all(mu > 0));}
 bool Family::Gamma::valideta (const arma::mat & eta) const {return true;}
+arma::mat Family::Gamma::devresid (const arma::mat & y, const arma::mat & mu) const {
+    return - 2 * (arma::log(y / mu) - (y - mu) / mu);
+}
 arma::mat Family::Gamma::devresid (const arma::mat & y, const arma::mat & mu, const arma::mat & wt) const {
     return - 2 * wt % (arma::log(y / mu) - (y - mu) / mu);
 }
