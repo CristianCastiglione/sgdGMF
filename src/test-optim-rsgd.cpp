4020a22b12d65e27f1490c495f2b2285bfdc863d
CristianCastiglione <cristian_castiglione@libero.it>
2024-01-28 18:34:34 +0100

    Create test-optim-rsgd.cpp

---
+++ src/test-optim-rsgd.cpp
@@ -0,0 +1,53 @@
+// test-optim-bsgd.cpp
+// author: Cristian Castiglione
+// creation: 27/01/2024
+// last change: 27/01/2024
+
+#include "optim.h"
+
+using namespace glm;
+
+// [[Rcpp::export]]
+Rcpp::List c_fit_rsgd (
+    const arma::mat & Y, 
+    const arma::mat & X, const arma::mat & B, 
+    const arma::mat & A, const arma::mat & Z,
+    const arma::mat & U, const arma::mat & V,
+    const std::string & familyname,
+    const std::string & linkname, 
+    const int & ncomp, 
+    const arma::vec & lambda,
+    const int & maxiter = 1000,
+    const double & eps = 0.01,
+    const int & nafill = 10,
+    const double & tol = 1e-08,
+    const int & size1 = 100,
+    const int & size2 = 100,
+    const double & burn = 0.75,
+    const double & rate0 = 0.01,
+    const double & decay = 0.01,
+    const double & damping = 1e-03,
+    const double & rate1 = 0.95,
+    const double & rate2 = 0.99,
+    const bool & parallel = false,
+    const int & nthreads = 1,
+    const bool & verbose = true,
+    const int & frequency = 250,
+    const bool & progress = false
+) {
+    arma::mat y = Y;
+
+    // Instantiate the parametrized family object
+    std::unique_ptr<Family> family = make_family(familyname, linkname);
+    
+    // Instantiate the Newton optimizer
+    RSGD sgd(
+        maxiter, eps, nafill, tol, size1, size2, burn, rate0, decay, 
+        damping, rate1, rate2, parallel, nthreads, verbose, frequency, progress);
+
+    // Perform the optimization via Newton algorithm
+    Rcpp::List output = sgd.fit(y, X, B, A, Z, U, V, family, ncomp, lambda);
+
+    // Return the estimated model
+    return output;
+}
