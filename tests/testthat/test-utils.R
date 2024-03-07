a88800fa63e180e0a7bc181ec727dcbf36214701
CristianCastiglione <cristian_castiglione@libero.it>
2024-03-07 22:54:17 +0100

    Update test-utils.R

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -3,15 +3,13 @@
 # creation: 05/02/2024
 # last change: 25/02/2024
 
-testthat::test_that("UV orthogonalization", {
-  n = 100; m = 10; d = 3
+testthat::test_that("QR orthogonalization", {
+  n = 100; m = 10; d = 5
 
-  old = list(
-    U = matrix(rnorm(n*d), nrow = n, ncol = d),
-    V = matrix(rnorm(m*d), nrow = m, ncol = d))
-  new = correct.uv(old$U, old$V)
+  old = list(U = matrix(rnorm(n*d), n, d), V = matrix(rnorm(m*d), m, d))
+  new = normalize.uv(old$U, old$V, method = "qr")
 
-  # Check if the matrix reconstraction is the same
+  # Check if the matrix reconstruction is the same
   testthat::expect_equal(tcrossprod(new$U, new$V), tcrossprod(old$U, old$V))
   # Check if the orthogonalized U has identity variance
   testthat::expect_equal(var(new$U), diag(d))
@@ -19,3 +17,19 @@ testthat::test_that("UV orthogonalization", {
   testthat::expect_equal(new$V[upper.tri(new$V)], rep(0, floor(d * (d-1) / 2)))
 })
 
+testthat::test_that("SVD orthogonalization", {
+  n = 100; m = 10; d = 5
+
+  old = list(U = matrix(rnorm(n*d), n, d), V = matrix(rnorm(m*d), m, d))
+  new = normalize.uv(old$U, old$V, method = "svd")
+  svds = RSpectra::svds(tcrossprod(new$U, new$V), d)$d
+
+  # Check if the matrix reconstruction is the same
+  testthat::expect_equal(tcrossprod(new$U, new$V), tcrossprod(old$U, old$V))
+  # Check if U is a scaled orthogonal matrix
+  testthat::expect_equal(crossprod(new$U), diag(d))
+  # Check if V is a scaled orthogonal matrix
+  testthat::expect_equal(crossprod(new$V), diag(colSums(new$V^2)))
+  # Check if U and V have the column norm
+  testthat::expect_equal(colSums(new$V^2), RSpectra::svds(tcrossprod(new$U, new$V), d)$d^2)
+})
