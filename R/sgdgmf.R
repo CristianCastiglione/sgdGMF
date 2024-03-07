6e53b9e87ebfab0c1e057e01345cbe3fea68c6f0
CristianCastiglione <cristian_castiglione@libero.it>
2024-03-07 09:31:03 +0100

    Update sgdgmf.R

--- R/sgdgmf.R
+++ R/sgdgmf.R
@@ -10,8 +10,7 @@
 #' @param Z matrix of column fixed effects (\eqn{q \times m})
 #' @param family a family as in the \code{\link{glm}} interface
 #' @param ncomp number of random effects to estimate (default 2)
-#' @param intercept should the model include a row/col intercept? (default FALSE)
-#' @param dispersion should the method estimate column-specific disperssion parameters? (default FALSE)
+#' @param selection ...
 #' @param penalty list of penalty parameters corresponding to u, v, a and b
 #' @param init list of initialization options
 #' @param control list of optimization options
@@ -37,64 +36,67 @@ sgdgmf = function (
     Z = NULL,
     family = poisson(),
     ncomp = 2,
-    method = "newton",
-    intercept = c(FALSE, FALSE),
-    dispersion = FALSE,
+    method = c("airwls", "newton", "msgd", "csgd", "rsgd", "csgd"),
     selection = FALSE,
     penalty = list(),
     init = list(),
-    control = list()) {
-
-  # Chose the optimizer
-  gmf.fit = NULL
-  methods = c("airwls", "newton", "m-sgd", "b-sgd", "c-sgd")
-  if (method %in% methods) {
-    if (method == "airwls") gmf.fit = gmf.airwls
-    if (method == "newton") gmf.fit = gmf.newton
-    if (method == "m-sgd") gmf.fit = gmf.memo.sgd
-    if (method == "b-sgd") gmf.fit = gmf.block.sgd
-    if (method == "c-sgd") gmf.fit = gmf.coord.sgd
-  } else {
-    stop("Method = `", method,"' is not implemented yet.")
-  }
-
-  # Sanity check for the intercept parameter
-  if (is.logical(intercept)) {
-    if (length(intercept) < 2) intercept = rep(intercept, 2)
-    if (length(intercept) > 2) intercept = intercept[1:2]
-  } else {
-    stop("Intercept must be a logical vector of two components.")
-  }
+    control = list()
+) {
 
   # Sanity check for the selection parameter
   if (!is.logical(selection)) {
     stop("Selection must be a logical parameter.")
   }
 
+  # Data dimensions
+  n = nrow(Y)
+  m = ncol(Y)
+  p = ifelse(is.null(X), 0, ncol(X))
+  q = ifelse(is.null(Z), 0, ncol(Z))
+  f = 0.3
+
   # Perform model selection minimizing the test deviance minimization
   if (selection) {
-    n = nrow(Y)
-    m = ncol(Y)
-    p = 0.3
     maxcomp = ncomp
-    data = partition(Y, p)
+    data = partition(Y, f)
     dev = numeric(maxcomp-1)
+    aic = numeric(maxcomp-1)
+    bic = numeric(maxcomp-1)
+
+    # Estimation loop
     for (ncomp in 2:maxcomp) {
-      mu = gmf.fit(
-        Y = data$train, X = X, Z = Z, family = family, ncomp = ncomp,
-        intercept = intercept, dispersion = dispersion,
-        penalty = penalty, init = init, control = control)$pred$mu
-      dev[ncomp-1] = matrix.deviance(mu = mu, y = data$test, family = family)
-      dev[ncomp-1] = dev[ncomp-1] / floor(p*n*m)
+      cat("Matrix rank:", ncomp, "\n")
+
+      # Effective number of parameters
+      df = m * p + n * q + (n + m) * ncomp
+
+      # Estimated mean matrix
+      mu = sgdgmf.fit(Y = Y, X = X, Z = Z, family = family, ncomp = ncomp,
+        method = method, penalty = penalty, init = init, control = control)$mu
+
+      # Train and test deviances
+      dev.train = matrix.deviance(mu = mu, y = data$train, family = family)
+      dev.test = matrix.deviance(mu = mu, y = data$test, family = family)
+
+      # Out-of-sample error and information criteria
+      dev[ncomp-1] = dev.test / (f*n*m)
+      aic[ncomp-1] = (dev.train + 2 * df) / ((1-f)*n*m)
+      bic[ncomp-1] = (dev.train + 2 * df * log((1-f)*n*m)) / ((1-f)*n*m)
     }
+
+    cat("Final estimation \n")
+
+    # Rank selection
     ncomp = which.min(dev)+1
   }
 
   # Fit the model using the chosen optimizer
-  fit = gmf.fit(
-    Y = Y, X = X, Z = Z, family = family, ncomp = ncomp,
-    intercept = intercept, dispersion = dispersion,
-    penalty = penalty, init = init, control = control)
+  fit = sgdgmf.fit(Y = Y, X = X, Z = Z, family = family, ncomp = ncomp,
+    method = method, penalty = penalty, init = init, control = control)
+
+  if (selection) {
+    fit$selection = cbind(ncomp = 2:maxcomp, dev = dev, aic = aic, bic = bic)
+  }
 
   # Convert the result in a S3 class
   class(fit) = "sgdgmf"
