50e5e3adb9c298ab3581d586b90adce7e6fbc632
CristianCastiglione <cristian_castiglione@libero.it>
2024-03-08 12:43:52 +0100

    Update test-control.R

--- tests/testthat/test-control.R
+++ tests/testthat/test-control.R
@@ -3,3 +3,26 @@
 # creation: 05/02/2024
 # last change: 05/02/2024
 
+testthat::test_that("Set AIRWLS control parameters", {
+  control = set.control("airwls", list())
+})
+
+testthat::test_that("Set Newton control parameters", {
+  control = set.control("newton", list())
+})
+
+testthat::test_that("Set M-SGD control parameters", {
+  control = set.control("msgd", list())
+})
+
+testthat::test_that("Set C-SGD control parameters", {
+  control = set.control("csgd", list())
+})
+
+testthat::test_that("Set R-SGD control parameters", {
+  control = set.control("rsgd", list())
+})
+
+testthat::test_that("Set B-SGD control parameters", {
+  control = set.control("bsgd", list())
+})
