8ad9e6aaf37316ba8816213c4036a5a0859672d2
CristianCastiglione <cristian_castiglione@libero.it>
2024-03-12 11:36:36 +0100

    Create sgdGMF-package.R

---
+++ R/sgdGMF-package.R
@@ -0,0 +1,17 @@
+#' @keywords internal
+"_PACKAGE"
+
+#' @useDynLib sgdGMF, .registration=TRUE
+#' @importFrom Rcpp evalCpp
+#' @exportPattern "^[[:alpha:]]+"
+#' @import Rcpp
+#' @import RcppArmadillo
+#' @importFrom stats glm.fit
+#' @importFrom svd propack.svd
+#' @importFrom RSpectra eigs svds
+#' @importFrom vegan procrustes
+#' @importFrom whitening whiteningMatrix
+#' @importFrom parallel detectCores makeCluster stopCluster
+#' @importFrom doParallel registerDoParallel
+#' @importFrom foreach %do% %dopar% foreach
+NULL
