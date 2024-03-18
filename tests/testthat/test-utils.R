34f54607266e73eb3af589127c75381d4b006922
CristianCastiglione <cristian_castiglione@libero.it>
2024-03-18 23:18:15 +0100

    Update test-utils.R

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -3,6 +3,29 @@
 # creation: 05/02/2024
 # last change: 25/02/2024
 
+testthat::test_that("Whitening matrix", {
+  n = 100; m = 10; d = 5
+
+  U = matrix(rnorm(n*d), n, d)
+  V = matrix(rnorm(d*d), d, d)
+  X = U %*% V
+  S = cov(X)
+  W.zca = whitening.matrix(cov(X), method = "ZCA")
+  W.pca = whitening.matrix(cov(X), method = "PCA")
+  W.zca.cor = whitening.matrix(cov(X), method = "ZCA-cor")
+  W.pca.cor = whitening.matrix(cov(X), method = "ZCA-cor")
+  W.chol = whitening.matrix(cov(X), method = "Cholesky")
+
+  # Check if the whitening matrix corresponds to the inverse covariance
+  testthat::expect_equal(cov(X %*% W.zca), diag(d))
+  testthat::expect_equal(crossprod(W.zca), solve(S))
+  testthat::expect_equal(crossprod(W.pca), solve(S))
+  testthat::expect_equal(crossprod(W.zca.cor), solve(S))
+  testthat::expect_equal(crossprod(W.pca.cor), solve(S))
+  testthat::expect_equal(crossprod(W.chol), solve(S))
+})
+
+
 testthat::test_that("QR orthogonalization", {
   n = 100; m = 10; d = 5
 
