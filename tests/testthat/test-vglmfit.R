5ec7e6b57279a8a1dd949191b6f1ec5a395ff473
CristianCastiglione <cristian_castiglione@libero.it>
2024-03-24 10:30:29 +0100

    Update test-vglmfit.R

--- tests/testthat/test-vglmfit.R
+++ tests/testthat/test-vglmfit.R
@@ -1,9 +1,9 @@
 # file: test-vglmfit.R
 # author: Cristian Castiglione
 # creation: 23/03/2024
-# last change: 023/03/2024
+# last change: 24/03/2024
 
-testthat::test_that("VOLS fitting", {
+testthat::test_that("Multivariate OLS fitting", {
   n = 100; m = 10; p = 5; q = p+1
 
   O = matrix(rexp(n*m, rate = 2.0), nrow = n, ncol = m)
@@ -13,8 +13,8 @@ testthat::test_that("VOLS fitting", {
   Y = O + tcrossprod(X, B) + E
 
   B.hat = ols.fit.coef(Y, X, gaussian(), offset = O)
-  F.hat = tcrossprod(X, B.hat)
-  E.hat = Y - O - F.hat
+  F.hat = O + tcrossprod(X, B.hat)
+  E.hat = Y - F.hat
 
   # Check the dimension and the basic properties of the estimates
   testthat::expect_equal(c(m, q), dim(B.hat))
@@ -24,7 +24,7 @@ testthat::test_that("VOLS fitting", {
 })
 
 
-testthat::test_that("VGLM fitting", {
+testthat::test_that("Binomial VGLM fitting", {
   n = 100; m = 10; p = 5; q = p+1
   family = binomial(link = "probit")
 
@@ -34,6 +34,52 @@ testthat::test_that("VGLM fitting", {
   eta = O + tcrossprod(X, B)
   mu = family$linkinv(eta)
   Y = matrix(rbinom(n*m, size = 1, prob = mu), nrow = n, ncol = m)
+
+  B.hat = vglm.fit.coef(Y, X, family, offset = O, parallel = FALSE)
+  eta.hat = O + tcrossprod(X, B.hat)
+  mu.hat = family$linkinv(eta.hat)
+  dmu.hat = family$mu.eta(eta.hat)
+  var.hat = family$variance(mu.hat)
+  res.hat = (Y - mu.hat) * dmu.hat / var.hat
+
+  # Check the dimension and the basic properties of the estimates
+  testthat::expect_equal(c(m, q), dim(B.hat))
+  testthat::expect_true(mean(crossprod(X, res.hat)) < 1e-04)
+})
+
+testthat::test_that("Poisson VGLM fitting", {
+  n = 100; m = 10; p = 5; q = p+1
+  family = poisson(link = "log")
+
+  O = matrix(rexp(n*m, rate = 2.0), nrow = n, ncol = m)
+  X = cbind(1, matrix(rnorm(n*p, mean = 0.0, sd = 1.0),  nrow = n, ncol = p))
+  B = matrix(rnorm(m*q, mean = 0.1, sd = 0.25), nrow = m, ncol = q)
+  eta = O + tcrossprod(X, B)
+  mu = family$linkinv(eta)
+  Y = matrix(rpois(n*m, lambda = mu), nrow = n, ncol = m)
+
+  B.hat = vglm.fit.coef(Y, X, family, offset = O, parallel = FALSE)
+  eta.hat = O + tcrossprod(X, B.hat)
+  mu.hat = family$linkinv(eta.hat)
+  dmu.hat = family$mu.eta(eta.hat)
+  var.hat = family$variance(mu.hat)
+  res.hat = (Y - mu.hat) * dmu.hat / var.hat
+
+  # Check the dimension and the basic properties of the estimates
+  testthat::expect_equal(c(m, q), dim(B.hat))
+  testthat::expect_true(mean(crossprod(X, res.hat)) < 1e-04)
+})
+
+testthat::test_that("Gamma VGLM fitting", {
+  n = 100; m = 10; p = 5; q = p+1
+  family = Gamma(link = "log")
+
+  O = matrix(rexp(n*m, rate = 2.0), nrow = n, ncol = m)
+  X = cbind(1, matrix(rnorm(n*p, mean = 0.0, sd = 1.0),  nrow = n, ncol = p))
+  B = matrix(rnorm(m*q, mean = 0.1, sd = 0.25), nrow = m, ncol = q)
+  eta = O + tcrossprod(X, B)
+  mu = family$linkinv(eta)
+  Y = matrix(rgamma(n*m, shape = 1, rate = mu), nrow = n, ncol = m)
 
   B.hat = vglm.fit.coef(Y, X, family, offset = O, parallel = FALSE)
   eta.hat = O + tcrossprod(X, B.hat)
