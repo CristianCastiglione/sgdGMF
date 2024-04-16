29c14f84d785ec4061f25ad8e15c66a732bbc674
CristianCastiglione <cristian_castiglione@libero.it>
2024-04-16 23:12:40 +0200

    Update vglmfit.R

--- R/vglmfit.R
+++ R/vglmfit.R
@@ -1,9 +1,16 @@
 
 #' @title Estimate the coefficients of a multivariate linear model
-#' @description ...
+#'
+#' @description
+#' Estimate the coefficients of a multivariate linear model via ordinary least squares.
+#'
+#' @param Y \eqn{n \times m} matrix of response variables
+#' @param X \eqn{n \times p} matrix of covariates
+#' @param offset \eqn{n \times m} matrix of offset values
+#'
 #' @keywords internal
 ols.fit.coef = function (
-    Y, X, family, offset = NULL
+    Y, X, offset = NULL
 ) {
   # Set the offset matrix
   if (is.null(offset)) offset = 0
@@ -18,7 +25,20 @@ ols.fit.coef = function (
 }
 
 #' @title Estimate the coefficients of a vector generalized linear model
-#' @description ...
+#'
+#' @description
+#' Estimate the coefficients of a vector generalized linear model via parallel
+#' iterative re-weighted least squares. Computations can be performed in parallel
+#' to speed up the execution.
+#'
+#' @param Y \eqn{n \times m} matrix of response variables
+#' @param X \eqn{n \times p} matrix of covariates
+#' @param family a \code{glm} family (see \code{\link{family}} for more details)
+#' @param offset \eqn{n \times m} matrix of offset values
+#' @param parallel if \code{TRUE}, allows for parallel computing using the \code{foreach} package
+#' @param nthreads number of cores to be used in parallel (only if \code{parallel=TRUE})
+#' @param clust registered cluster to be used for distributing the computations (only if \code{parallel=TRUE})
+#'
 #' @keywords internal
 vglm.fit.coef = function (
     Y, X, family = gaussian(), offset = NULL,
