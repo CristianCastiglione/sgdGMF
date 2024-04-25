8653c33add0bdc0d40f6be32adf743341223279d
CristianCastiglione <cristian_castiglione@libero.it>
2024-04-25 12:06:41 +0200

    Update sgdGMF-package.R

--- R/sgdGMF-package.R
+++ R/sgdGMF-package.R
@@ -6,17 +6,37 @@
 #' @exportPattern "^[[:alpha:]]+"
 #' @import Rcpp
 #' @import RcppArmadillo
-#' @importFrom stats glm.fit family
-#' @importFrom stats gaussian quasi
-#' @importFrom stats binomial quasibinomial
-#' @importFrom stats poisson quasipoisson
-#' @importFrom stats Gamma inverse.gaussian
-#' @importFrom stats biplot screeplot heatmap
-#' @importFrom MASS neg.bin negative.binomial
-#' @importFrom generics refit
-#' @importFrom stats heatmap
-#' @importFrom RSpectra svds eigs eigs_sym
-#' @importFrom parallel detectCores makeCluster stopCluster
+#' @importFrom stats glm.fit
+#' @importFrom stats family
+#' @importFrom stats gaussian
+#' @importFrom stats binomial
+#' @importFrom stats poisson
+#' @importFrom stats Gamma
+#' @importFrom stats inverse.gaussian
+#' @importFrom stats quasi
+#' @importFrom stats quasibinomial
+#' @importFrom stats quasipoisson
+#' @importFrom MASS neg.bin
+#' @importFrom MASS negative.binomial
+#' @importFrom RSpectra svds
+#' @importFrom RSpectra eigs
+#' @importFrom RSpectra eigs_sym
+#' @importFrom parallel detectCores
+#' @importFrom parallel makeCluster
+#' @importFrom parallel stopCluster
 #' @importFrom doParallel registerDoParallel
-#' @importFrom foreach %do% %dopar% foreach
+#' @importFrom foreach %do%
+#' @importFrom foreach %dopar%
+#' @importFrom foreach foreach
+#' @importFrom stats fitted
+#' @importFrom stats predict
+#' @importFrom stats coef coefficients
+#' @importFrom stats resid residuals
+#' @importFrom stats deviance
+#' @importFrom stats BIC
+#' @importFrom stats deviance
+#' @importFrom stats biplot
+#' @importFrom stats screeplot
+#' @importFrom graphics image
+#' @importFrom generics refit
 NULL
