237eb89b17ed5d274b74919f08d6a1636939490a
CristianCastiglione <cristian_castiglione@libero.it>
2024-10-08 12:08:24 +0200

    Fix R CMD warnings

--- src/test-family.cpp
+++ src/test-family.cpp
@@ -7,105 +7,105 @@
 
 using namespace glm;
 
-// [[Rcpp::export("pcc.gaussian.variance")]]
+// [[Rcpp::export("cpp.gaussian.variance")]]
 arma::vec cpp_gaussian_variance (const arma::vec & mu) {
     std::unique_ptr<Link> ptr = std::make_unique<Identity>();
     Gaussian f(ptr);
     return f.variance(mu);
 }
 
-// [[Rcpp::export("pcc.gaussian.initialize")]]
+// [[Rcpp::export("cpp.gaussian.initialize")]]
 arma::vec cpp_gaussian_initialize (const arma::vec & y) {
     std::unique_ptr<Link> ptr = std::make_unique<Identity>();
     Gaussian f(ptr);
     return f.initialize(y);
 }
 
-// [[Rcpp::export("pcc.gaussian.devresid")]]
+// [[Rcpp::export("cpp.gaussian.devresid")]]
 arma::vec cpp_gaussian_devresid (const arma::vec & y, const arma::vec & mu) {
     std::unique_ptr<Link> ptr = std::make_unique<Identity>();
     Gaussian f(ptr);
     return f.devresid(y, mu);
 }
 
-// [[Rcpp::export("pcc.binomial.variance")]]
+// [[Rcpp::export("cpp.binomial.variance")]]
 arma::vec cpp_binomial_variance (const arma::vec & mu) {
     std::unique_ptr<Link> ptr = std::make_unique<Logit>();
     Binomial f(ptr);
     return f.variance(mu);
 }
 
-// [[Rcpp::export("pcc.binomial.initialize")]]
+// [[Rcpp::export("cpp.binomial.initialize")]]
 arma::vec cpp_binomial_initialize (const arma::vec & y) {
     std::unique_ptr<Link> ptr = std::make_unique<Logit>();
     Binomial f(ptr);
     return f.initialize(y);
 }
 
-// [[Rcpp::export("pcc.binomial.devresid")]]
+// [[Rcpp::export("cpp.binomial.devresid")]]
 arma::vec cpp_binomial_devresid (const arma::vec & y, const arma::vec & mu) {
     std::unique_ptr<Link> ptr = std::make_unique<Logit>();
     Binomial f(ptr);
     return f.devresid(y, mu);
 }
 
-// [[Rcpp::export("pcc.poisson.variance")]]
+// [[Rcpp::export("cpp.poisson.variance")]]
 arma::vec cpp_poisson_variance (const arma::vec & mu) {
     std::unique_ptr<Link> ptr = std::make_unique<Log>();
     Poisson f(ptr);
     return f.variance(mu);
 }
 
-// [[Rcpp::export("pcc.poisson.initialize")]]
+// [[Rcpp::export("cpp.poisson.initialize")]]
 arma::vec cpp_poisson_initialize (const arma::vec & y) {
     std::unique_ptr<Link> ptr = std::make_unique<Log>();
     Poisson f(ptr);
     return f.initialize(y);
 }
 
-// [[Rcpp::export("pcc.poisson.devresid")]]
+// [[Rcpp::export("cpp.poisson.devresid")]]
 arma::vec cpp_poisson_devresid (const arma::vec & y, const arma::vec & mu) {
     std::unique_ptr<Link> ptr = std::make_unique<Log>();
     Poisson f(ptr);
     return f.devresid(y, mu);
 }
 
-// [[Rcpp::export("pcc.gamma.variance")]]
+// [[Rcpp::export("cpp.gamma.variance")]]
 arma::vec cpp_gamma_variance (const arma::vec & mu) {
     std::unique_ptr<Link> ptr = std::make_unique<Log>();
     Gamma f(ptr);
     return f.variance(mu);
 }
 
-// [[Rcpp::export("pcc.gamma.initialize")]]
+// [[Rcpp::export("cpp.gamma.initialize")]]
 arma::vec cpp_gamma_initialize (const arma::vec & y) {
     std::unique_ptr<Link> ptr = std::make_unique<Log>();
     Gamma f(ptr);
     return f.initialize(y);
 }
 
-// [[Rcpp::export("pcc.gamma.devresid")]]
+// [[Rcpp::export("cpp.gamma.devresid")]]
 arma::vec cpp_gamma_devresid (const arma::vec & y, const arma::vec & mu) {
     std::unique_ptr<Link> ptr = std::make_unique<Log>();
     Gamma f(ptr);
     return f.devresid(y, mu);
 }
 
-// [[Rcpp::export("pcc.negbinom.variance")]]
+// [[Rcpp::export("cpp.negbinom.variance")]]
 arma::vec cpp_negbinom_variance (const arma::vec & mu) {
     std::unique_ptr<Link> ptr = std::make_unique<Log>();
     NegativeBinomial f(ptr);
     return f.variance(mu);
 }
 
-// [[Rcpp::export("pcc.negbinom.initialize")]]
+// [[Rcpp::export("cpp.negbinom.initialize")]]
 arma::vec cpp_negbinom_initialize (const arma::vec & y) {
     std::unique_ptr<Link> ptr = std::make_unique<Log>();
     NegativeBinomial f(ptr);
     return f.initialize(y);
 }
 
-// [[Rcpp::export("pcc.negbinom.devresid")]]
+// [[Rcpp::export("cpp.negbinom.devresid")]]
 arma::vec cpp_negbinom_devresid (const arma::vec & y, const arma::vec & mu) {
     std::unique_ptr<Link> ptr = std::make_unique<Log>();
     NegativeBinomial f(ptr);
